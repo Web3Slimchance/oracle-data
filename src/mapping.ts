58dc725f381874ba315dc7ec777e564b0e56cd3a
Remy <lapin.vert@gmail.com>
2020-12-08 02:10:06 +0900

    Change Schema naming in mapping file to avoid naming conflicts

--- src/mapping.ts
+++ src/mapping.ts
@@ -4,13 +4,13 @@ import {
   Transfer,
   Approval
 } from "../generated/LinkToken/LinkToken"
-import { Transfer, Approval } from "../generated/schema"
+import * as schema from "../generated/schema"
 
 export function handleTransfer(event: Transfer): void {
-  let transfer = Transfer.load(event.params._amount.toHex())
+  let transfer = schema.Transfer.load(event.params._amount.toHex())
 
   if (transfer == null) {
-    transfer = new Transfer(event.params._amount.toHex())
+    transfer = new schema.Transfer(event.params._amount.toHex())
     transfer.count = BigInt.fromI32(0)
   }
 
@@ -23,10 +23,10 @@ export function handleTransfer(event: Transfer): void {
 
 
 export function handleApproval(event: Approval): void {
-  let approval = Approval.load(event.params._amount.toHex())
+  let approval = schema.Approval.load(event.params._amount.toHex())
 
   if (approval == null) {
-    approval = new Approval(event.params._amount.toHex())
+    approval = new schema.Approval(event.params._amount.toHex())
     approval.count = BigInt.fromI32(0)
   }
 
